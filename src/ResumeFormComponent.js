9e34308e11f6fc6bd54fe71df0bb04d4231ed0c6
indhu-github <indhusworkmail@gmail.com>
2021-03-05 22:01:57 +0530

    updated

--- src/ResumeFormComponent.js
+++ src/ResumeFormComponent.js
@@ -109,6 +109,12 @@ function ResumeFormComponent() {
     <>
       {!formSubmitted ? (
         <>
+          <div>
+            <h1 className="text-center p-3">Quick Resume</h1>
+            <h3 className="text-center text-warning p-3">
+              Build your own resume by filling the details and download it!!!
+            </h3>
+          </div>
           <div className="MainForm">
             {currentPage > 1 ? (
               <IconButton
